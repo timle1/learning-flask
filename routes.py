9c8766231942fdcb6f84cd35c2da1e7e87639f82
timle1 <boston1re@gmail.com>
2017-07-28 08:19:28 -0400

    add authorization to routes

--- routes.py
+++ routes.py
@@ -27,6 +27,9 @@ def about():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if 'email' in session:
+        return redirect(url_for('home'))
+
     form = SignupForm()
 
     if request.method == "POST":
@@ -45,24 +48,27 @@ def signup():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-  form = LoginForm()
-
-  if request.method == "POST":
-    if form.validate() == False:
-      return render_template("login.html", form=form)
-    else:
-      email = form.email.data
-      password = form.password.data
-
-      user = User.query.filter_by(email=email).first()
-      if user is not None and user.check_password(password):
-        session['email'] = form.email.data
+    if 'email' in session:
         return redirect(url_for('home'))
-      else:
-        return redirect(url_for('login'))
 
-  elif request.method == 'GET':
-    return render_template('login.html', form=form)
+    form = LoginForm()
+
+    if request.method == "POST":
+        if form.validate() == False:
+            return render_template("login.html", form=form)
+        else:
+            email = form.email.data
+            password = form.password.data
+
+        user = User.query.filter_by(email=email).first()
+        if user is not None and user.check_password(password):
+            session['email'] = form.email.data
+            return redirect(url_for('home'))
+        else:
+            return redirect(url_for('login'))
+
+    elif request.method == 'GET':
+        return render_template('login.html', form=form)
 
 @app.route('/logout')
 def logout():
@@ -70,8 +76,10 @@ def logout():
     return redirect(url_for('index'))
 
 @app.route('/home')
-
 def home():
+    if 'email' not in session:
+        return redirect(url_for('login'))
+
     return render_template("home.html")
 
 
