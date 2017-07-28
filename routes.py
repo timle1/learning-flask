6c1f76df02b866c472d32e2df68a0b12e49c873e
timle1 <boston1re@gmail.com>
2017-07-27 23:25:30 -0400

    add signup form

--- routes.py
+++ routes.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
-from models import db
+from flask import Flask, render_template, request
+from models import db, User
+from forms import SignupForm
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+POSTGRES = {
+    'user': 'PGuser',
+    'pw': 'PGpass',
+    'db': 'learningflask',
+    'host': 'localhost',
+    'port': '5432',
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 db.init_app(app)
+
+app.secret_key = "development-key"
 
 @app.route("/")
 def index():
@@ -13,6 +24,22 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+
+    if request.method == "POST":
+        if form.validate() == False:
+            return render_template('signup.html', form=form)
+        else:
+            newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
+            return 'Success!'
+    elif request.method == "GET":
+        return render_template('signup.html', form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
