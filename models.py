638b676ce90417ecc88e3c58ed3cd911d27fd9e4
timle1 <boston1re@gmail.com>
2017-07-28 09:50:01 -0400

    add geocoding

--- models.py
+++ models.py
@@ -1,6 +1,12 @@
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug import generate_password_hash, check_password_hash
 
+import geocoder
+import urllib3
+import json
+from urllib.parse import urljoin
+
+
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -22,3 +28,49 @@ class User(db.Model):
 
   def check_password(self, password):
     return check_password_hash(self.pwdhash, password)
+
+# p = Place()
+# places = p.query("1600 Amphitheater Parkway Mountain View CA")
+class Place(object):
+  def meters_to_walking_time(self, meters):
+    # 80 meters is one minute walking time
+    return int(meters / 80)  
+
+  def wiki_path(self, slug):
+    return urljoin("http://en.wikipedia.org/wiki/", slug.replace(' ', '_'))
+  
+  def address_to_latlng(self, address):
+    g = geocoder.google(address)
+    return (g.lat, g.lng)
+
+  def query(self, address):
+    lat, lng = self.address_to_latlng(address)
+    
+    query_url = 'https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=5000&gscoord={0}%7C{1}&gslimit=20&format=json'.format(lat, lng)
+    http = urllib3.PoolManager()
+    response = http.request('GET', query_url)
+    results = response.data
+
+    data = json.loads(results)
+    
+    places = []
+    for place in data['query']['geosearch']:
+      name = place['title']
+      meters = place['dist']
+      lat = place['lat']
+      lng = place['lon']
+
+      wiki_url = self.wiki_path(name)
+      walking_time = self.meters_to_walking_time(meters)
+
+      d = {
+        'name': name,
+        'url': wiki_url,
+        'time': walking_time,
+        'lat': lat,
+        'lng': lng
+      }
+
+      places.append(d)
+
+    return places
--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from models import db, User
-from forms import SignupForm, LoginForm
+from models import db, User, Place
+from forms import SignupForm, LoginForm, AddressForm
 
 app = Flask(__name__)
 
@@ -75,12 +75,33 @@ def logout():
     session.pop('email', None)
     return redirect(url_for('index'))
 
-@app.route('/home')
+@app.route("/home", methods=["GET", "POST"])
 def home():
-    if 'email' not in session:
-        return redirect(url_for('login'))
+  if 'email' not in session:
+    return redirect(url_for('login'))
 
-    return render_template("home.html")
+  form = AddressForm()
+
+  places = []
+  my_coordinates = (37.4221, -122.0844)
+
+  if request.method == 'POST':
+    if form.validate() == False:
+      return render_template('home.html', form=form)
+    else:
+      # get the address
+      address = form.address.data 
+
+      # query for places around it
+      p = Place()
+      my_coordinates = p.address_to_latlng(address)
+      places = p.query(address)
+
+      # return those results
+      return render_template('home.html', form=form, my_coordinates=my_coordinates, places=places)
+
+  elif request.method == 'GET':
+    return render_template("home.html", form=form, my_coordinates=my_coordinates, places=places)
 
 
 if __name__ == "__main__":
